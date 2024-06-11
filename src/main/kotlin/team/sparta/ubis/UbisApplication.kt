626f138e2493da32294e0d3035392589dd65d955
npureaun <98468118+npureaun@users.noreply.github.com>
2024-06-11 19:55:39 +0900

    first Commit

---
+++ src/main/kotlin/team/sparta/ubis/UbisApplication.kt
@@ -0,0 +1,11 @@
+package team.sparta.ubis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UbisApplication
+
+fun main(args: Array<String>) {
+    runApplication<UbisApplication>(*args)
+}
---
+++ src/test/kotlin/team/sparta/ubis/UbisApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.sparta.ubis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UbisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
