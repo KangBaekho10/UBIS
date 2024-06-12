f9b296caba5552e616753332ab7224558d0bbb9f
eunhye han <hellou8363@gmail.com>
2024-06-12 13:11:53 +0900

    feat: 회원 정보 조회 Controller 코드 추가

--- src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
@@ -1,5 +1,21 @@
 package org.ubis.ubis.domain.member.controller
 
-class MemberController {
-    // TODO
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.ubis.ubis.domain.member.dto.MemberResponse
+import org.ubis.ubis.domain.member.service.MemberService
+
+@RestController
+@RequestMapping("/members")
+class MemberController(
+    private val memberService: MemberService
+) {
+
+    @GetMapping
+    fun getMember() : ResponseEntity<MemberResponse> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.getMember())
+    }
 }
