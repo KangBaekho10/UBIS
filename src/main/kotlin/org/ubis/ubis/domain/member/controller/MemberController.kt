fdf43e72c98ee108ba709f8a82b5cab0b9fe62fe
eunhye han <hellou8363@gmail.com>
2024-06-12 14:34:25 +0900

    feat: 특정 사용자 정보 조회를 위한 파라미터 추가 jwt 토큰 구현 후 제거해야 함

--- src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.ubis.ubis.domain.member.dto.MemberResponse
 import org.ubis.ubis.domain.member.service.MemberService
@@ -14,8 +15,9 @@ class MemberController(
     private val memberService: MemberService
 ) {
 
+    // TODO: security 구현 완료 후 파라미터 변경
     @GetMapping
-    fun getMember() : ResponseEntity<MemberResponse> {
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.getMember())
+    fun getMember(@RequestParam memberId: Long): ResponseEntity<MemberResponse> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.getMember(memberId))
     }
 }
