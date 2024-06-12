b3b0961e104508179c4ee2da84a86e30ede08dfd
eunhye han <hellou8363@gmail.com>
2024-06-12 14:58:35 +0900

    feat: MemberRepository에서 회원 정보를 가져오는 코드 추가

--- src/main/kotlin/org/ubis/ubis/domain/member/service/MemberService.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/service/MemberService.kt
@@ -1,5 +1,19 @@
 package org.ubis.ubis.domain.member.service
 
-class MemberService {
-    // TODO
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.ubis.ubis.domain.exception.ModelNotFoundException
+import org.ubis.ubis.domain.member.dto.MemberResponse
+import org.ubis.ubis.domain.member.model.toResponse
+import org.ubis.ubis.domain.member.repository.MemberRepository
+
+@Service
+class MemberService(
+    private val memberRepository: MemberRepository
+) {
+    fun getMember(memberId: Long): MemberResponse {
+        val member = memberRepository.findByIdOrNull(memberId) ?: throw ModelNotFoundException("Member", memberId)
+
+        return member.toResponse()
+    }
 }
