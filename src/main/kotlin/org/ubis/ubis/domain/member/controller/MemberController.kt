561d3ee703d58c141ac9a1702c99ed72c135e6c9
eunhye han <hellou8363@gmail.com>
2024-06-12 19:32:13 +0900

    feat: 회원정보 수정 Controller에 @Valid 추가

--- src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
@@ -3,13 +3,7 @@ package org.ubis.ubis.domain.member.controller
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.ubis.ubis.domain.member.dto.CreateMemberRequest
 import org.ubis.ubis.domain.member.dto.MemberResponse
 import org.ubis.ubis.domain.member.dto.UpdateMemberRequest
@@ -35,8 +29,10 @@ class MemberController(
 
     // TODO: security 구현 완료 후 파라미터 변경
     @PostMapping("/password-check")
-    fun passwordCheck(@RequestParam memberId: Long,
-                      @RequestBody password: String): ResponseEntity<Unit> {
+    fun passwordCheck(
+        @RequestParam memberId: Long,
+        @RequestBody password: String
+    ): ResponseEntity<Unit> {
         memberService.passwordCheck(memberId, password)
         return ResponseEntity.status(HttpStatus.OK).build()
     }
@@ -44,7 +40,7 @@ class MemberController(
     @PutMapping
     fun updateMember(
         @RequestParam memberId: Long,
-        @RequestBody updateMemberRequest: UpdateMemberRequest
+        @Valid @RequestBody updateMemberRequest: UpdateMemberRequest
     ): ResponseEntity<MemberResponse> {
         return ResponseEntity.status(HttpStatus.OK).body(memberService.updateMember(memberId, updateMemberRequest))
     }
