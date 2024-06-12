985d45137abd14d95b02c3e12a6d1bf3a4b7d9e8
eunhye han <hellou8363@gmail.com>
2024-06-12 16:20:59 +0900

    feat: 회원정보 수정 요청 Controller 코드 추가

--- src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/controller/MemberController.kt
@@ -3,10 +3,13 @@ package org.ubis.ubis.domain.member.controller
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.ubis.ubis.domain.member.dto.MemberResponse
+import org.ubis.ubis.domain.member.dto.UpdateMemberRequest
 import org.ubis.ubis.domain.member.service.MemberService
 
 @RestController
@@ -19,5 +22,14 @@ class MemberController(
     @GetMapping
     fun getMember(@RequestParam memberId: Long): ResponseEntity<MemberResponse> {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.getMember(memberId))
+    }
+
+    // TODO: 비밀번호를 확인하는 함수 필요
+
+    @PutMapping
+    fun updateMember(
+        @RequestParam memberId: Long,
+        @RequestBody updateMemberRequest: UpdateMemberRequest) : ResponseEntity<MemberResponse> {
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.updateMember(memberId, updateMemberRequest))
     }
 }
