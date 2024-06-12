1ec8cb5d40d40f7c623a5228e9f8fc5deabde98d
eunhye han <hellou8363@gmail.com>
2024-06-12 11:28:25 +0900

    feat: Member Entity 추가

--- src/main/kotlin/org/ubis/ubis/domain/member/model/Member.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/model/Member.kt
@@ -1,5 +1,19 @@
 package org.ubis.ubis.domain.member.model
 
-class Member {
-    // TODO
+import jakarta.persistence.*
+
+class Member(
+    val email: String,
+    var name: String,
+    var password: String,
+
+    @Column(name = "phone_number")
+    var phoneNumber: String,
+
+    @Enumerated(EnumType.STRING)
+    var role: Role = Role.CUSTOMER
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
 }
