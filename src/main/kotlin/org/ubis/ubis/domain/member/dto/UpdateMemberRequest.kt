d27f404ddf4198b8742d4f02eb1f0e89df271a17
eunhye han <hellou8363@gmail.com>
2024-06-12 19:30:52 +0900

    feat: 회원정보 수정 요청 시 비밀번호 검증을 위한 정규표현식 추가

--- src/main/kotlin/org/ubis/ubis/domain/member/dto/UpdateMemberRequest.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/dto/UpdateMemberRequest.kt
@@ -1,7 +1,13 @@
 package org.ubis.ubis.domain.member.dto
 
+import jakarta.validation.constraints.Pattern
+
 data class UpdateMemberRequest(
     val name: String?,
     val phoneNumber: String?,
+    @field:Pattern(
+        regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\\^\$*.\\[\\]{}\\(\\)\\?\\-\\\"!@#%&/\\\\,><':;|_~`]).{8,15}\$",
+        message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자를 포함해야 합니다."
+    )
     val password: String?
 )
