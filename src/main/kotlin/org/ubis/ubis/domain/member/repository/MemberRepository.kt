4af11f75274fe4cff257e3981729b18bfa7ce789
eunhye han <hellou8363@gmail.com>
2024-06-12 17:04:08 +0900

    fix: DB에 이미 존재하는 전화번호인지 체크하는 코드 추가

--- src/main/kotlin/org/ubis/ubis/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/repository/MemberRepository.kt
@@ -3,4 +3,8 @@ package org.ubis.ubis.domain.member.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.ubis.ubis.domain.member.model.Member
 
-interface MemberRepository : JpaRepository<Member, Long>
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun existsByEmail(email: String): Boolean
+
+    fun existsByPhoneNumber(phoneNumber: String): Boolean
+}
