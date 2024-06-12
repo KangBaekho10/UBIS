9cede933a74fd35bbbfed749bb6a1114599e7884
eunhye han <hellou8363@gmail.com>
2024-06-12 16:04:13 +0900

    feat: 회원 정보를 수정하는 Service 코드 추가 - name, phoneNumber, password 중 일부만 변경될 수 있으므로 DTO 파라미터를 nullable로 설정 - 해당 파라미터가 null이 아닌 경우에만 값 변경 - 이전 비밀번호 이력을 가져와서 수정 요청한 비밀번호와 일치하는 경우 예외 처리 - 이전 비밀번호 이력에 없는 경우 수정 - 변경된 정보를 응답으로 반환(비밀번호 제외)

--- src/main/kotlin/org/ubis/ubis/domain/member/service/MemberService.kt
+++ src/main/kotlin/org/ubis/ubis/domain/member/service/MemberService.kt
@@ -2,8 +2,11 @@ package org.ubis.ubis.domain.member.service
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.ubis.ubis.domain.exception.ModelNotFoundException
+import org.ubis.ubis.domain.exception.ReusedPasswordException
 import org.ubis.ubis.domain.member.dto.MemberResponse
+import org.ubis.ubis.domain.member.dto.UpdateMemberRequest
 import org.ubis.ubis.domain.member.model.toResponse
 import org.ubis.ubis.domain.member.repository.MemberRepository
 
@@ -13,6 +16,46 @@ class MemberService(
 ) {
     fun getMember(memberId: Long): MemberResponse {
         val member = memberRepository.findByIdOrNull(memberId) ?: throw ModelNotFoundException("Member", memberId)
+
+        return member.toResponse()
+    }
+
+    @Transactional
+    fun updateMember(
+        memberId: Long,
+        updateMemberRequest: UpdateMemberRequest
+    ): MemberResponse {
+        val member = memberRepository.findByIdOrNull(memberId) ?: throw ModelNotFoundException("Member", memberId)
+
+        if (updateMemberRequest.name != null) {
+            member.name = updateMemberRequest.name
+        }
+
+        if (updateMemberRequest.phoneNumber != null) {
+            member.phoneNumber = updateMemberRequest.phoneNumber
+        }
+
+        if (updateMemberRequest.password != null) {
+            // 이전에 사용했던 비밀번호 이력을 가져옴
+            val pwHistory = member.pwHistory.split(",").toMutableList() // [hh23,  dddd,  1234]
+
+            // 수정 요청한 비밀번호가 이전에 사용했던 적이 있는지 확인
+            if (pwHistory.contains(updateMemberRequest.password)) {
+                throw ReusedPasswordException("이전에 사용했던 비밀번호는 사용할 수 없습니다.")
+            }
+
+            // 일치하는 비밀번호가 없으면 기존 변경 이력횟수를 확인
+            if (pwHistory.size >= 3) { // 3번 이상이면
+                // 첫 번째 요소(가장 오래 전에 사용했던 비밀번호)를 제거
+                pwHistory.removeFirst()
+            }
+
+            pwHistory.add(updateMemberRequest.password) // TODO: passwordEncode로 변경 후 add 해야 함
+
+            // 변경된 비밀번호 List를 비밀번호 이력에 추가
+            member.pwHistory = pwHistory.joinToString(",")
+            member.password = pwHistory.last()
+        }
 
         return member.toResponse()
     }
