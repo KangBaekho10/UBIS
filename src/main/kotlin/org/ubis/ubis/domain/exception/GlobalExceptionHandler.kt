0050abf696175f0214778540b068f5e4012b1d9c
eunhye han <hellou8363@gmail.com>
2024-06-12 19:02:29 +0900

    feat: 비밀번호 불일치에 대한 에외 추가

--- src/main/kotlin/org/ubis/ubis/domain/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/ubis/ubis/domain/exception/GlobalExceptionHandler.kt
@@ -2,6 +2,7 @@ package org.ubis.ubis.domain.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -30,6 +31,11 @@ class GlobalExceptionHandler {
             .body(ErrorResponse(e.getBindingResult().getFieldErrors().map {
                 "${it.field}: ${it.defaultMessage}"
             }.joinToString(",")))
+    }
+
+    @ExceptionHandler(BadCredentialsException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse(e.message))
     }
 
 
