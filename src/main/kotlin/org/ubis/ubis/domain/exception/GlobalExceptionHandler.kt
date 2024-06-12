dbd18e2d9a6c5702ae229c114b5417b4f585593f
eunhye han <hellou8363@gmail.com>
2024-06-12 15:04:42 +0900

    feat: 회원ID로 회원정보를 가져올 때 해당하는 회원정보가 없을 경우에 대한 예외 처리

---
+++ src/main/kotlin/org/ubis/ubis/domain/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,15 @@
+package org.ubis.ubis.domain.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleModelNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse(e.message))
+    }
+}
