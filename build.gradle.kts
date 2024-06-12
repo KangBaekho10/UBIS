286a388da8c8901fa9ada836d9b972f997521690
eunhye han <hellou8363@gmail.com>
2024-06-12 14:22:35 +0900

    chore: Entity 설정을 위한 allopen, noarg 플러그인 코드 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.noarg") version "1.9.23"
 }
 
 group = "team.sparta.ubis"
@@ -22,6 +23,18 @@ configurations {
 
 repositories {
     mavenCentral()
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 val kotestVersion = "5.5.5"
