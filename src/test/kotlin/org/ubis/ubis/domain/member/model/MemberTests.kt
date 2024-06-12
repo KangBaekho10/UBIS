14c8d3dadce12b620998957c4a8b0a5a81978ac9
eunhye han <hellou8363@gmail.com>
2024-06-12 13:07:34 +0900

    feat: Member Entity 내 response 함수에 대한 테스트 코드 추가

--- src/test/kotlin/org/ubis/ubis/domain/member/model/MemberTests.kt
+++ src/test/kotlin/org/ubis/ubis/domain/member/model/MemberTests.kt
@@ -7,9 +7,10 @@ import io.kotest.matchers.shouldBe
 class MemberTests : BehaviorSpec({
 
     Given("Member Information") {
+        val id = 1L
         val email = "test@test.com"
         val name = "test user"
-        val password = "1234"
+        val password = "!a123456"
         val phoneNumber = "010-1234-5678"
 
         When("Member Constructor") {
@@ -26,6 +27,25 @@ class MemberTests : BehaviorSpec({
                 result.password shouldBe password
                 result.phoneNumber shouldBe phoneNumber
                 result.role shouldBe Role.CUSTOMER
+            }
+        }
+
+        When("Member Response") {
+            val result = Member(
+                email = email,
+                name = name,
+                password = password,
+                phoneNumber = phoneNumber
+            ).toResponse()
+
+            Then("Result should be") {
+                // id는 val로 수정 불가라 Member.toResponse에서 id값을 임의로 1L로 지정한 상태에서 테스트 했음
+//                result.id shouldBe id
+                result.email shouldBe email
+                result.name shouldBe name
+                result.phoneNumber shouldBe phoneNumber
+
+                println(result)
             }
         }
     }
