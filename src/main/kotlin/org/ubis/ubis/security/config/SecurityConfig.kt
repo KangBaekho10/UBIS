a49c5011edf3e296a2a803caea8aad5495bc4e43
KangBaekho <xodhks6740@gmail.com>
2024-06-12 10:40:59 +0900

    Feat: Password 암호화 및 swagger 로그인 예외 처리

---
+++ src/main/kotlin/org/ubis/ubis/security/config/SecurityConfig.kt
@@ -0,0 +1,32 @@
+package org.ubis.ubis.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers(
+                    "/members/**",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                ).permitAll()
+                    .anyRequest().authenticated()
+            }
+
+            .build()
+    }
+}
